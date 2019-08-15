c58b32bb1ab87680a5bd4ef5f245de03f33cdaf0
Memhir-YasuE <habesha787@gmail.com>
2019-08-15 17:19:32 -0400

    The python end can process on flask

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from flask import Flask, make_response, request,render_template
 import pandas as pd
+from mainProcessor import Automater
+
+
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,9 +17,16 @@ def process_view():
     if not request.files:
         return "No file"
     files = request.files.getlist("data_files")
-    data_xls = pd.read_csv(files[0])
+    # Data processing stage goes under here!
+    robo = Automater()
+    df_files_list = robo.htm_to_df(files)
+    clean_df_list = robo.pre_process_df(df_files_list)
+    concatinated_df = robo.concatinate_df(clean_df_list)
+    return concatinated_df.to_html()
 
-    return data_xls.to_html()
+    # data_xls = pd.read_csv(files[0])
+
+    # return data_xls.to_html()
 
 
 if __name__ == '__main__':
